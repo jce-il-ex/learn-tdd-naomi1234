27c80d4502c2b2a04c35b7d31a3a9e51ca68c11b
naomi1234 <naomiyg@gmail.com>
2017-01-10 18:55:59 +0200

    GREEN: real test

--- cash.js
+++ cash.js
@@ -1,4 +1,4 @@
-/*var C = {};     // C Object simplifies exporting the module
+var C = {};     // C Object simplifies exporting the module
 module.exports = C;            // export the module with a single method
 C.coins = [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
 C.getChange = function (totalPayable, cashPaid) {
@@ -20,10 +20,4 @@ C.getChange = function (totalPayable, cashPaid) {
         }
     }
     return change;
-};*/
-var C = {};                    // C Object simplifies exporting the module
-C.getChange = function (totalPayable, cashPaid) {
-    'use strict';
-    return [50, 20, 20];    // just enough to pass :-)
-}
-module.exports = C;   
+};  
--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ change       = [500,10,2,2]  // £5, 10p, 2p, 2p
 
 describe('Cash Register', function(){
   describe('Module C', function(){
-/*it('getChange(210,300) should equal [50,20,20]', function(){
+it('getChange(210,300) should equal [50,20,20]', function(){
     assert.deepEqual(C.getChange(210,300), [50,20,20]);
 });
 it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
@@ -16,13 +16,7 @@ it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
 });
 it('getChange(1487,10000) should equal [5000, 2000, 1000, 500, 10, 2, 1 ]', function(){
     assert.deepEqual(C.getChange(1487,10000), [5000, 2000, 1000, 500, 10, 2, 1 ]);
-});*/
-it('getChange(210,300) should equal [50,20,20]', function(){
-    assert.deepEqual(C.getChange(210,300), [50,20,20]);
 });
-it('getChange(486,1000) should equal [500, 10, 2, 2]', function(){
-    assert.deepEqual(C.getChange(486,1000), [500, 10, 2, 2]);
-});
- 
+
     });
   });
