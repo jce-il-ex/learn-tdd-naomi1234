27b4cc573ed0e5fbc53294ba5b8d7ea4cbab2a29
naomi1234 <naomiyg@gmail.com>
2017-01-10 18:50:40 +0200

    GREEN: first test

--- cash.js
+++ cash.js
@@ -21,3 +21,9 @@ C.getChange = function (totalPayable, cashPaid) {
     }
     return change;
 };*/
+var C = {};                    // C Object simplifies exporting the module
+C.getChange = function () {    // enough to satisfy the test
+    'use strict';
+    return true;               // also passes JSLint
+};
+module.exports = C;   
